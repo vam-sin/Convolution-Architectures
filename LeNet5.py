c43285f5d431acbde40f75a53d17b4e235dce5fa
vam-sin <vamsinallapareddy@gmail.com>
2019-05-30 11:14:40 +0530

    LeNet addition

--- LeNet5.py
+++ LeNet5.py
@@ -11,12 +11,12 @@ def LeNet():
     model.add(Conv2D(6,kernel_size=(5,5),strides=(1,1),padding='valid',
                      activation='relu',input_shape=(32,32,3)))
     # Layer S2
-    model.add(AveragePooling2D(pool_size=2,strides=None,padding='valid'))
+    model.add(AveragePooling2D(pool_size=2,strides=2,padding='valid'))
     # Layer C3
     model.add(Conv2D(16,kernel_size=(5,5),strides=(1,1),padding='valid',
                      activation='relu'))
     # Layer S4
-    model.add(AveragePooling2D(pool_size=2,strides=None,padding='valid'))
+    model.add(AveragePooling2D(pool_size=2,strides=2,padding='valid'))
     # Fully Connected Layer
     model.add(Flatten())
     model.add(Dense(120,activation='relu'))
