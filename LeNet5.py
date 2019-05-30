eb7e4fe4b32a0f1ea55b21bfe11f550569ee978d
vam-sin <vamsinallapareddy@gmail.com>
2019-05-30 11:05:27 +0530

    LeNet addition

---
+++ LeNet5.py
@@ -0,0 +1,33 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, AveragePooling2D
+from keras.regularizers import l2
+from keras.layers.normalization import BatchNormalization
+import keras
+
+def LeNet():
+    model = Sequential()
+    # Layer C1
+    model.add(Conv2D(6,kernel_size=(5,5),strides=(1,1),padding='valid',
+                     activation='relu',input_shape=(32,32,3)))
+    # Layer S2
+    model.add(AveragePooling2D(pool_size=2,strides=None,padding='valid'))
+    # Layer C3
+    model.add(Conv2D(16,kernel_size=(5,5),strides=(1,1),padding='valid',
+                     activation='relu'))
+    # Layer S4
+    model.add(AveragePooling2D(pool_size=2,strides=None,padding='valid'))
+    # Fully Connected Layer
+    model.add(Flatten())
+    model.add(Dense(120,activation='relu'))
+    model.add(Dense(84,activation='relu'))
+    model.add(Dense(10,activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
+    model.summary()
+    
+    return model
+
+classifier = LeNet()
